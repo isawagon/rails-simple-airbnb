9b72fce25fe85d5ca9f4ef1237ff82a9a79d9782
isabelle levy <isabel.levy@icloud.com>
2022-04-21 20:58:42 +0200

    first view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Vue magnifique Martinique',
+  address: "O'Mullane, Le Diamant",
+  description: 'Grande Villa vue sur la mer, piscine, 5 chambres ',
+  price_per_night: 350,
+  number_of_guests: 10
+)
+Flat.create!(
+  name: 'Bel appartement Le Diamant ',
+  address: '10 boulevard Saint-Michel',
+  description: 'Très bel appartement, vue sur la baie du Diamant.',
+  price_per_night: 60,
+  number_of_guests: 2
+)
+
